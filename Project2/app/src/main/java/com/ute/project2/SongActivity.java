23b67e0f8c507c9c0741317b2f29219df755226e
phuong090802 <20110547@student.hcmute.edu.vn>
2023-03-19 16:07:16 +0700

    4:05 PM 3/19/2023

--- Project2/app/src/main/java/com/ute/project2/SongActivity.java
+++ Project2/app/src/main/java/com/ute/project2/SongActivity.java
@@ -2,43 +2,50 @@ package com.ute.project2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.slider.Slider;
 import com.ute.project2.model.Song;
 
-public class SongActivity extends AppCompatActivity implements BottomNavigationListener {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
+public class SongActivity extends AppCompatActivity {
+
+    MaterialToolbar tbTop;
+    ImageView ivSongImage;
     BottomNavigationView bottomNavigationView;
-    BottomNavigationManager bottomNavigationManager;
+    TextView tvCountDown;
+    TextView tvEndTime;
+    Slider sliderTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song);
+        tbTop = findViewById(R.id.tbTop);
+        ivSongImage = findViewById(R.id.ivSongImage);
         bottomNavigationView = findViewById(R.id.bottom_navigation_song);
-        bottomNavigationManager = new BottomNavigationManager(this, bottomNavigationView);
-        SongFragment fragment = new SongFragment();
+        bottomNavigationView.setSelectedItemId(R.id.play_pause);
+        tvCountDown = findViewById(R.id.tvCountDown);
+        tbTop = findViewById(R.id.tbTop);
+        tvEndTime = findViewById(R.id.tvEndTime);
+        sliderTime = findViewById(R.id.sliderTime);
         Intent intent = getIntent();
         Song song = (Song) intent.getSerializableExtra("song");
-        Bundle bundle = new Bundle();
-        bundle.putSerializable("song", song);
-        fragment.setArguments(bundle);
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout_song, fragment).commit();
+        tbTop.setTitle(song.getSongName());
+        tbTop.setSubtitle(song.getSingerName());
+        ivSongImage.setImageResource(song.getSongImage());
+        DateFormat formatter = new SimpleDateFormat("mm:ss", Locale.getDefault());
+        String time = formatter.format(new Date(song.getSongTime()));
+        tvEndTime.setText(time);
     }
 
-    @Override
-    public void onBottomNavigationItemSelected(int itemId) {
-        Fragment selectedFragment = null;
-        if (itemId == R.id.search) {
-            selectedFragment = new SearchFragment();
-        } else if (itemId == R.id.yourLibrary) {
-            selectedFragment = new YourLibraryFragment();
-        }
-        if (selectedFragment != null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout_song, selectedFragment).commit();
-        }
-    }
 }
--- Project2/app/src/main/java/com/ute/project2/database/Database.java
+++ Project2/app/src/main/java/com/ute/project2/database/Database.java
@@ -11,29 +11,29 @@ public class Database {
     private static final List<Song> songList = new ArrayList<>();
 
     public static List<Song> getSongList() {
-        songList.add(new Song("Neil", R.drawable.song, R.raw.file_example, 2, "Dax", "Skyler"));
-        songList.add(new Song("Salem", R.drawable.song, R.raw.file_example, 2, "Dax", "Mohammed"));
-        songList.add(new Song("Remi", R.drawable.song, R.raw.file_example, 2, "Dax", "Dennis"));
-        songList.add(new Song("Liam", R.drawable.song, R.raw.file_example, 2, "Dax", "Nixon"));
-        songList.add(new Song("Noah", R.drawable.song, R.raw.file_example, 2, "Dax", "Rex"));
-        songList.add(new Song("Oliver", R.drawable.song, R.raw.file_example, 2, "Dax", "Uriah"));
-        songList.add(new Song("Elijah", R.drawable.song, R.raw.file_example, 2, "Dax", "Lee"));
-        songList.add(new Song("James", R.drawable.song, R.raw.file_example, 2, "Dax", "Louie"));
-        songList.add(new Song("William", R.drawable.song, R.raw.file_example, 2, "Dax", "Alberto"));
-        songList.add(new Song("Benjamin", R.drawable.song, R.raw.file_example, 2, "Dax", "Reese"));
-        songList.add(new Song("Lucas", R.drawable.song, R.raw.file_example, 2, "Dax", "Quinton"));
-        songList.add(new Song("Henry", R.drawable.song, R.raw.file_example, 2, "Dax", "Kingsley"));
-        songList.add(new Song("Theodore", R.drawable.song, R.raw.file_example, 2, "Francis", "Chaim"));
-        songList.add(new Song("Jack", R.drawable.song, R.raw.file_example, 2, "Francis", "Alfredo"));
-        songList.add(new Song("Levi", R.drawable.song, R.raw.file_example, 2, "Francis", "Mauricio"));
-        songList.add(new Song("Alexander", R.drawable.song, R.raw.file_example, 2, "Francis", "Caspian"));
-        songList.add(new Song("Jackson", R.drawable.song, R.raw.file_example, 2, "Francis", "Legacy"));
-        songList.add(new Song("Mateo", R.drawable.song, R.raw.file_example, 2, "Francis", "Ocean"));
-        songList.add(new Song("Daniel", R.drawable.song, R.raw.file_example, 2, "Francis", "Ozzy"));
-        songList.add(new Song("Michael", R.drawable.song, R.raw.file_example, 2, "Francis", "Briar"));
-        songList.add(new Song("Mason", R.drawable.song, R.raw.file_example, 2, "Francis", "Wilson"));
-        songList.add(new Song("Sebastian", R.drawable.song, R.raw.file_example, 2, "Francis", "Forest"));
-        songList.add(new Song("Ethan", R.drawable.song, R.raw.file_example, 2, "Francis", "Grey"));
+        songList.add(new Song("Neil", R.drawable.song, R.raw.file_example, 90000, "Dax", "Skyler"));
+        songList.add(new Song("Salem", R.drawable.song, R.raw.file_example, 90000, "Dax", "Mohammed"));
+        songList.add(new Song("Remi", R.drawable.song, R.raw.file_example, 90000, "Dax", "Dennis"));
+        songList.add(new Song("Liam", R.drawable.song, R.raw.file_example, 90000, "Dax", "Nixon"));
+        songList.add(new Song("Noah", R.drawable.song, R.raw.file_example, 90000, "Dax", "Rex"));
+        songList.add(new Song("Oliver", R.drawable.song, R.raw.file_example, 90000, "Dax", "Uriah"));
+        songList.add(new Song("Elijah", R.drawable.song, R.raw.file_example, 90000, "Dax", "Lee"));
+        songList.add(new Song("James", R.drawable.song, R.raw.file_example, 90000, "Dax", "Louie"));
+        songList.add(new Song("William", R.drawable.song, R.raw.file_example, 90000, "Dax", "Alberto"));
+        songList.add(new Song("Benjamin", R.drawable.song, R.raw.file_example, 90000, "Dax", "Reese"));
+        songList.add(new Song("Lucas", R.drawable.song, R.raw.file_example, 90000, "Dax", "Quinton"));
+        songList.add(new Song("Henry", R.drawable.song, R.raw.file_example, 90000, "Dax", "Kingsley"));
+        songList.add(new Song("Theodore", R.drawable.song, R.raw.file_example, 90000, "Francis", "Chaim"));
+        songList.add(new Song("Jack", R.drawable.song, R.raw.file_example, 90000, "Francis", "Alfredo"));
+        songList.add(new Song("Levi", R.drawable.song, R.raw.file_example, 90000, "Francis", "Mauricio"));
+        songList.add(new Song("Alexander", R.drawable.song, R.raw.file_example, 90000, "Francis", "Caspian"));
+        songList.add(new Song("Jackson", R.drawable.song, R.raw.file_example, 90000, "Francis", "Legacy"));
+        songList.add(new Song("Mateo", R.drawable.song, R.raw.file_example, 90000, "Francis", "Ocean"));
+        songList.add(new Song("Daniel", R.drawable.song, R.raw.file_example, 90000, "Francis", "Ozzy"));
+        songList.add(new Song("Michael", R.drawable.song, R.raw.file_example, 90000, "Francis", "Briar"));
+        songList.add(new Song("Mason", R.drawable.song, R.raw.file_example, 90000, "Francis", "Wilson"));
+        songList.add(new Song("Sebastian", R.drawable.song, R.raw.file_example, 90000, "Francis", "Forest"));
+        songList.add(new Song("Ethan", R.drawable.song, R.raw.file_example, 90000, "Francis", "Grey"));
         return songList;
     }
 
